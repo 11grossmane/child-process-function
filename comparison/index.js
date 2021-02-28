f37eb85dcb630cd9ae2c73ac7933a8d06b47a324
Eric Grossman <eric.josh.grossman@gmail.com>
2021-02-28 00:17:58 -0600

    comparison package.json files and no undefined args

---
+++ comparison/index.js
@@ -0,0 +1,77 @@
+const spawn = require('../src')
+
+const asyncFunction = async () => {
+    let count = 0
+    while (count < 100000) count++
+    console.log('inside async function')
+}
+
+const main = () => {
+    console.log('\n')
+    //notice how we aren't awaiting
+    //shouldnt this mean we get the
+    //async log first?
+    asyncFunction()
+    console.log('after')
+}
+
+main()
+
+const mainTwo = () => {
+    console.log('\n')
+
+    //same result as above, even though this is
+    //still utilizing event loop
+    new Promise((res, rej) => {
+        let count = 0
+        while (count < 100000) count++
+        console.log('inside promise')
+    })
+    console.log('after')
+}
+
+mainTwo()
+
+const mainThree = () => {
+    console.log('\n')
+
+    //the timer part of setTimeout is actually happening inside a child process that is running in parallel...this is why code can execute while the timer is counting down
+    //when the timer is finished, the callback gets put on event queue, and will execute when our call stack is empty?
+    setTimeout(() => {
+        console.log('inside timeout')
+    }, 200)
+    console.log('after')
+}
+
+//mainThree()
+
+const mainFour = () => {
+    console.log('\n')
+
+    //the timer part of setTimeout is actually happening inside a child process that is running in parallel...this is why code can execute while the timer is counting down
+    //when the timer is finished, the callback gets put on event queue, and will execute when our call stack is empty?
+    let cont = false
+    setTimeout(() => {
+        console.log('inside timeout')
+        cont = true
+    }, 200)
+    console.log('after')
+
+    //callback never executed because this function cannot leave the callstack
+    while (!cont) {}
+}
+
+//mainFour()
+
+const mainFive = () => {
+    console.log('\n')
+
+    spawn(() => {
+        let count = 0
+        while (count < 100000) count++
+        console.log('inside child process')
+    })
+    console.log('after')
+}
+
+mainFive()
